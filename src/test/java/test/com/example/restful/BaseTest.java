0e96bbda75013c1af5d25d3a9ec6e6ca348f999b
m91snik <g.m91snik@gmail.com>
2013-06-22 19:00:31 +0400

    Removed @Ignore and make base test abstract

--- src/test/java/test/com/example/restful/BaseTest.java
+++ src/test/java/test/com/example/restful/BaseTest.java
@@ -1,36 +1,32 @@
 package test.com.example.restful;
 
+import com.example.restful.dom.User;
 import junit.framework.Assert;
-
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.example.restful.dom.User;
-
-@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext-test.xml")
-public class BaseTest {
+public abstract class BaseTest {
 
-	protected User createTestUser(String postfix) {
-		User user = new User();
-		user.setEmail("ex@mail.com" + postfix);
-		user.setPassword("password" + postfix);
-		user.setFirstName("first name" + postfix);
-		user.setLastName("last name" + postfix);
-		return user;
-	}
+    protected User createTestUser(String postfix) {
+        User user = new User();
+        user.setEmail("ex@mail.com" + postfix);
+        user.setPassword("password" + postfix);
+        user.setFirstName("first name" + postfix);
+        user.setLastName("last name" + postfix);
+        return user;
+    }
 
-	protected User createTestUser() {
-		return createTestUser("");
-	}
+    protected User createTestUser() {
+        return createTestUser("");
+    }
 
-	protected void assertUsersEqualsExceptId(User expected, User real) {
-		Assert.assertEquals(expected.getEmail(), real.getEmail());
-		Assert.assertEquals(expected.getPassword(), real.getPassword());
-		Assert.assertEquals(expected.getFirstName(), real.getFirstName());
-		Assert.assertEquals(expected.getLastName(), real.getLastName());
-	}
+    protected void assertUsersEqualsExceptId(User expected, User real) {
+        Assert.assertEquals(expected.getEmail(), real.getEmail());
+        Assert.assertEquals(expected.getPassword(), real.getPassword());
+        Assert.assertEquals(expected.getFirstName(), real.getFirstName());
+        Assert.assertEquals(expected.getLastName(), real.getLastName());
+    }
 }
